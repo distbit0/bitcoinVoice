41fed4ab71f1c077a2b66863fd1cdf79573432d2
Alex... Pi <randomiseme33@gmail.com>
2017-09-20 06:57:02 +1000

    Update blockchainInterface.py

--- backend/blockchainInterface.py
+++ backend/blockchainInterface.py
@@ -3,6 +3,7 @@ def searchLabels(startPos, endPos, startDate, endDate, searchTerm, coin):
     pass
   else:
     pass
+  return [{"rank":1, "label":"I like big blocks and I cannot lie", "utxo":1000}, {"rank":2, "label":"Vote Trump", "utxo":999}, {"rank":3, "label":"Bitcoin will reach $1,000,000 by yesterday", "utxo":998}, {"rank":4, "label":"This is a website...", "utxo":997}, {"rank":5, "label":"Big blockers are snazzy", "utxo":996}, {"rank":6, "label":"America should adopt Vote Flux", "utxo":995}, {"rank":7, "label":"Vote Hillary :/", "utxo":.00000001}]
 
     
   
