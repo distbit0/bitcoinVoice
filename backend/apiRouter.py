1bccb3e0733be1109f71bbd767886cdd3074ebde
Alex... Pi <randomiseme33@gmail.com>
2017-09-20 16:49:45 +1000

    Update apiRouter.py

--- backend/apiRouter.py
+++ backend/apiRouter.py
@@ -6,6 +6,7 @@ import tornado.web
 import tornado.httpserver
 import json
 frontendPath = sys.argv[1] + "/frontend"
+path = sys.argv[1]
 
 def getConfig():
   import json
@@ -28,11 +29,15 @@ class CSSHandler(tornado.web.RequestHandler):
     fileContents = open(frontendPath + "/css.css").read()
     self.write(fileContents)
 
+class redirect (tornado.web.RequestHandler):
+  def get(self):  
+    self.redirect("https://" +  self.request.host + self.request.uri)
+    
 class homeHandler(tornado.web.RequestHandler):
   # This routing table directs off to the services to support the UI.
   # Route to the appropriate function, passing in parameters as required.
-
-  def get(self):  
+  
+ def get(self):  
     function = self.get_argument("function")
     
     if function == "getUiDefaults":
@@ -49,19 +54,30 @@ class homeHandler(tornado.web.RequestHandler):
       labels = interface.searchLabels(startPos, endPos, startDate, endDate, searchTerm, coin)
       self.write(json.dumps(labels))
 
-
+redirectApplication = tornado.web.Application([
+        (r'/', redirect)
+    ])
 application = tornado.web.Application([
   (r"/api/", homeHandler),
-  (r"/", HTMLHandler),
+  (r"/*", HTMLHandler),
   (r"/js.js", JSHandler),
   (r"/css.css", CSSHandler),
   (r'/webfavicon.ico()', tornado.web.StaticFileHandler, {'path': frontendPath + '/favicon.ico'}),
   (r'/magnifyingGlass.png()', tornado.web.StaticFileHandler, {'path': frontendPath + '/magnifyingGlass.png'}),
   (r"/shareTech.ttf()", tornado.web.StaticFileHandler, {'path': frontendPath + '/shareTech.ttf'}),
   (r"/hairline.ttf()", tornado.web.StaticFileHandler, {'path': frontendPath + '/hairline.ttf'}),
-])
+], ssl_options={
+    "certfile": os.path.join(path + "/backend/ssl/certificate.crt"),
+    "keyfile": os.path.join(path + "/backend/ssl/private.key"),
+})
 
-http_server = tornado.httpserver.HTTPServer(application)
+http_server = tornado.httpserver.HTTPServer(application, ssl_options={
+    "certfile": os.path.join(path + "/backend/ssl/certificate.crt"),
+    "keyfile": os.path.join(path + "/backend/ssl/private.key"),
+})
+
+
 if __name__ == "__main__":
-	http_server.listen(80)
-	tornado.ioloop.IOLoop.instance().start()
+  redirectApplication.listen(80)
+  http_server.listen(443)
+  tornado.ioloop.IOLoop.instance().start()
