f6726fbbff1a86594c26841eade0eb8b39fc6b8b
pimania <randomiseme33@gmail.com>
2017-10-20 22:48:14 +1100

    Update to use bincoinUnlimited binary.

--- backend/start-bitcoin-cli-getinfo.sh
+++ backend/start-bitcoin-cli-getinfo.sh
@@ -2,10 +2,10 @@ echo "Bitcoin Cash mainnet"
 $HOME/bitcoinCashUnlimited/bin/bitcoin-cli -datadir=$HOME/.bitcoinCash getinfo
 
 echo "Bitcoin mainnet"
-bitcoin-cli -datadir=$HOME/.bitcoin getinfo
+$HOME/bitcoinUnlimited/bin/bitcoin-cli -datadir=$HOME/.bitcoin getinfo
 
 echo "Bitcoin testnet"
-bitcoin-cli -datadir=$HOME/.bitcoinTestnet getinfo
+$HOME/bitcoinUnlimited/bin/bitcoin-cli -datadir=$HOME/.bitcoinTestnet getinfo
 
 
 
--- backend/start-bitcoind.sh
+++ backend/start-bitcoind.sh
@@ -1,4 +1,4 @@
 # the ~ must be replaced with the complete path to the blockchain folders
 $HOME/bitcoinCashUnlimited/bin/bitcoind -datadir=$HOME/.bitcoinCash &
-bitcoind -datadir=$HOME/.bitcoin &
-bitcoind -datadir=$HOME/.bitcoinTestnet &
+$HOME/bitcoinUnlimited/bin/bitcoind -datadir=$HOME/.bitcoin &
+$HOME/bitcoinUnlimited/bin/bitcoind -datadir=$HOME/.bitcoinTestnet &
